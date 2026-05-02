33d3eebc1165b04e8d3d8f5e39bad671970257ad
Jolibois Towensia <tjolibois@gmail.com>
2026-05-02 14:29:45 -0400

    changements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
 import Sidebar from './components/Sidebar'
 import Navbar from './components/Navbar'
+
 import Accueil from './pages/Accueil'
 import Connexion from './pages/Connexion'
 import Register from './pages/Register'
@@ -8,32 +10,49 @@ import Profil from './pages/Profil'
 import Demandes from './pages/Demandes'
 import Messages from './pages/Messages'
 import Dashboard from './pages/Dashboard'
-import { useState } from 'react'
 
+/**
+ * Layout principal de l'application
+ * Contient Sidebar + Navbar + contenu des pages
+ */
 function Layout({ children }) {
   return (
     <div className="app-layout">
       <Sidebar />
+
       <div className="main-content">
         <Navbar />
-        <div className="page-content">{children}</div>
+
+        <div className="page-content">
+          {children}
+        </div>
       </div>
     </div>
   )
 }
 
+/**
+ * App principale avec routing
+ */
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Pages publiques */}
         <Route path="/connexion" element={<Connexion />} />
         <Route path="/register" element={<Register />} />
+
+        {/* Pages avec layout (sidebar + navbar) */}
         <Route path="/" element={<Layout><Accueil /></Layout>} />
         <Route path="/demandes" element={<Layout><Demandes /></Layout>} />
         <Route path="/messages" element={<Layout><Messages /></Layout>} />
         <Route path="/profil" element={<Layout><Profil /></Layout>} />
         <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+
+        {/* Redirection fallback */}
         <Route path="*" element={<Navigate to="/" />} />
+
       </Routes>
     </BrowserRouter>
   )
